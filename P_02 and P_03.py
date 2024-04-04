cb246ab46844b6423c339ccadaf06835b941e498
Para1ux <164087346+Para1ux@users.noreply.github.com>
2024-04-04 15:09:33 +1300

    Add files via upload

---
+++ P_02 and P_03.py
@@ -0,0 +1,15 @@
+username = input("what is your name? ")
+
+fav_num = int(input("What is your favorite number? "))
+
+double = fav_num * 2
+half = fav_num / 2
+square = fav_num * fav_num
+
+print(f"Hi {username}, your favourite number is {fav_num}")
+
+print(f"Double your favourite number is {double}")
+print(f"Half your favourite number is {half}")
+print(f"{fav_num} squared is {square}")
+print()
+print("Have a nice day!")
