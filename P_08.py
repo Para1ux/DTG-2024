9c5a5a0decb069487b2b43ceeb5e185610c1dc76
Para1ux <164087346+Para1ux@users.noreply.github.com>
2024-04-29 12:14:28 +1200

    Add files via upload

---
+++ P_08.py
@@ -0,0 +1,16 @@
+#PYTHON BASICS
+#Variable
+greeting = ""
+say_hello = "Hello"
+#Can't start with a number
+#Can't have spaces
+#Should have lowercase letters and underscores
+#Set up variables using '=', compare two values using '=='
+#== is equal to
+#!= is not equal to
+#< less than
+#> more than
+#<= less than or equal to
+#>= more than or equal to
+#indenting is used for loops, if statements, and functions
+#Functions is indented code that executes when the function is called or used
